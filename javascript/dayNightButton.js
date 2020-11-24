f0fe4cdc1b1049930d2a4277658633be773a751a
sir306 <hardingnick00@gmail.com>
2020-11-24 18:48:06 +1300

    commit

--- javascript/dayNightButton.js
+++ javascript/dayNightButton.js
@@ -1,6 +1,7 @@
 
 const bodyElement = document.querySelector("body");
 const aNavLinkElement = document.querySelector("nav");
+const aNavLink = document.querySelectorAll("a.nav-link");
 const divNavContainer = document.querySelector("div.nav-container");
 const h4BusinessTitle = document.querySelector("h4.business-title");
 const aNavLinkActive = document.querySelector("a.nav-link.active");
@@ -12,7 +13,7 @@ const divServices = document.querySelector("div.services");
 const tdServiceData = document.querySelector("td.service-data");
 //contact
 const tableContactTable = document.querySelector("table.contact-table");
-const tdContactTableData = document.querySelector("td.contact-table-data");
+const tdContactTableData = document.querySelectorAll("td.contact-table-data");
 const divContactDiv = document.querySelector("div.contact-div");
 const aContactNum = document.querySelector("a.contact-num");
 const aContactEmail = document.querySelector("a.contact-email");
@@ -20,46 +21,10 @@ const formEmailForm = document.querySelector("form.email-form");
 const emailText = document.querySelector("#email-text");
 const emailEmail = document.querySelector("#email-email");
 const inputFormBtn = document.querySelector("input.form-btn");
+const footer = document.querySelector("footer");
 //about us
-function setToNight(){
-    bodyElement.style.backgroundColor = "lightgrey";
-    aNavLinkElement.style.backgroundColor = "#933994";
-    divNavContainer.style.border = "blanchedalmond solid 2px";
-    h4BusinessTitle.style.color = "white";
-    aNavLinkActive.style.backgroundColor = "black";
-    //home
-    //imageCarousel.style.border = "solid 3px black";
-    //services
-    //tableServiceTable.style.color = "darkviolet";
-    //divServices.style.border = "solid black .2em";
-    //divServices.style.backgroundColor = "#933994";
-    //tdServiceData.style.border = "#933994";
-    //tdServiceData.style.backgroundColor = "darkgrey";
-    //contact
-    tableContactTable.style.color = "darkviolet";
-    tdContactTableData.style.border = "#933994";
-    tdContactTableData.style.backgroundColor = "darkgrey";
-    divContactDiv.style.border = "black solid .2em";
-    divContactDiv.style.backgroundColor = "#933994";
-    aContactNum.style.color = "darkviolet";
-    aContactEmail.style.color = "darkviolet";
-    formEmailForm.style.border = "solid black .2em";
-    formEmailForm.style.backgroundColor = "#933994";
-    emailText.style.color = "darkviolet";
-    emailText.style.backgroundColor = "darkgrey";
-    emailEmail.style.color = "darkviolet";
-    emailEmail.style.backgroundColor = "darkgrey";
-    aContactNum.addEventListener('mouseover', hoverChange);
-    aContactEmail.addEventListener('mouseover', hoverChange);
-    aContactNum.addEventListener('mouseout', hoverChangeOver);
-    aContactEmail.addEventListener('mouseout', hoverChangeOver);
-    inputFormBtn.style.color = "darkviolet";
-    inputFormBtn.style.backgroundColor = "darkgrey";
-    inputFormBtn.style.border = "blanchedalmond solid .15em";
-    inputFormBtn.addEventListener('mouseover', btnHoverChange);
-    inputFormBtn.addEventListener('mouseout', btnHoverChangeOver);
-    //about us
-}
+const divAboutUsLeftPanel = document.querySelector("div.about-us-left-panel");
+const divAboutUsRightPanel = document.querySelector("div.about-us-right-panel");
 
 function hoverChange() {
     aContactNum.style.color = "whitesmoke";
@@ -78,44 +43,82 @@ function btnHoverChange() {
 function btnHoverChangeOver(){
     inputFormBtn.style.backgroundColor = "darkviolet";
 }
+
+function setToNight(){
+    //Nav
+    bodyElement.style.backgroundColor = "lightgrey";
+    aNavLink.forEach(navlink => {
+        navlink.style.backgroundColor = "darkgrey";
+    });
+    
+
+    aNavLinkElement.style.backgroundColor = "rgb('102, 0, 255')";
+    divNavContainer.style.border = "blanchedalmond solid 2px";
+    h4BusinessTitle.style.color = "white";
+    aNavLinkActive.style.backgroundColor = "black";
+    //Footer
+    footer.style.color = "blanchedalmond";
+    footer.style.backgroundColor = "black";
+
+    //home
+    //imageCarousel.style.border = "solid 3px black";
+    //services
+    //tableServiceTable.style.color = "darkviolet";
+    //divServices.style.border = "solid black .2em";
+    //divServices.style.backgroundColor = "rgb('102, 0, 255')";
+    //tdServiceData.style.border = "rgb('102, 0, 255')";
+    //tdServiceData.style.backgroundColor = "darkgrey";
+    //contact
+    tableContactTable.style.color = "darkviolet";
+    tdContactTableData.forEach(tableData => {
+        tableData.style.border = "rgb('102, 0, 255')";
+        tableData.style.backgroundColor = "darkgrey";
+    });
+    
+    
+    divContactDiv.style.border = "black solid .2em";
+    divContactDiv.style.backgroundColor = "rgb('102, 0, 255')";
+    aContactNum.style.color = "darkviolet";
+    aContactEmail.style.color = "darkviolet";
+    formEmailForm.style.border = "solid black .2em";
+    formEmailForm.style.backgroundColor = "rgb('102, 0, 255')";
+    emailText.style.color = "darkviolet";
+    emailText.style.backgroundColor = "darkgrey";
+    emailEmail.style.color = "darkviolet";
+    emailEmail.style.backgroundColor = "darkgrey";
+    aContactNum.addEventListener('mouseover', hoverChange);
+    aContactEmail.addEventListener('mouseover', hoverChange);
+    aContactNum.addEventListener('mouseout', hoverChangeOver);
+    aContactEmail.addEventListener('mouseout', hoverChangeOver);
+    inputFormBtn.style.color = "darkviolet";
+    inputFormBtn.style.backgroundColor = "darkgrey";
+    inputFormBtn.style.border = "blanchedalmond solid .15em";
+    inputFormBtn.addEventListener('mouseover', btnHoverChange);
+    inputFormBtn.addEventListener('mouseout', btnHoverChangeOver);
+    //about us
+    // divAboutUsLeftPanel.style.border = "solid black .2em";
+    // divAboutUsLeftPanel.style.backgroundColor = "rgb('102, 0, 255')";
+    // divAboutUsRightPanel.style.border = "solid black .2em";
+    // divAboutUsRightPanel.style.backgroundColor = "rgb('102, 0, 255')";
+}
+
+function setToDay(){
+    bodyElement.style.backgroundColor = "rgb('255, 153, 255')";
+    //Nav
+    aNavLinkElement.style.backgroundColor = "rgb('102, 0, 255')";
+    divNavContainer.style.border = "blanchedalmond solid 2px";
+    h4BusinessTitle.style.color = "white";
+    aNavLinkActive.style.backgroundColor = "black";
+}
+
+
 document.querySelector("button.layout-toggle-night").addEventListener('click', setToNight);
-
-
-/* about us page styling */
-
-div.about-us-left-panel {
-    border: solid black .2em;
-    background-color: #933994;
-}
-
-div.about-us-right-panel {
-    border: solid black .2em;
-    background-color: #933994;
-}
-
-
-/* footers */
-
-footer {
-    color: blanchedalmond;
-    background-color: black;
-}
-
-//DAY
-/* Desktop Style Sheet */
-
-html {
-    background-color: #6600FF;
-}
-
-body {
-    background-color: #FF99FF;
-}
-
+document.querySelector("button.layout-toggle-day").addEventListener('click', setToDay);
++
 a.nav-link {
-    color: white !important;
+
     background-color: #6600FF;
-    transition: 0.3s;
+
     border: blanchedalmond .03em solid;
 }
 
@@ -132,128 +135,4 @@ nav {
 
 .navbar-toggler-icon {
     color: white !important;
-}
-
-div.nav-container {
-    border: blanchedalmond solid 2px;
-}
-
-h4.business-title {
-    color: white;
-}
-
-a.nav-link.active {
-    background-color: #66CCFF;
-}
-
-a.nav-link:hover {
-    opacity: 0.6;
-}
-
-
-/* image-carousel */
-
-#image-carousel {
-    border: solid 3px #6600FF;
-}
-
-
-/* services page */
-
-table.service-table {
-    color: white;
-}
-
-div.services {
-    border: solid #6600FF .2em;
-    background-color: #CC00FF;
-}
-
-td.service-data {
-    border: #CC00FF;
-    background-color: #6600FF;
-}
-
-
-/* contact table */
-
-table.contact-table {
-    color: white;
-}
-
-td.contact-table-data {
-    border: #CC00FF;
-    background-color: #6600FF;
-}
-
-div.contact-div {
-    border: #6600FF solid .2em;
-    background-color: #CC00FF;
-}
-
-a.contact-num {
-    color: white;
-}
-
-a.contact-num:hover {
-    color: #66CCFF;
-}
-
-a.contact-email {
-    color: white;
-}
-
-a.contact-email:hover {
-    color: #66CCFF;
-}
-
-
-/* email form */
-
-form.email-form {
-    border: solid #6600FF .2em;
-    background-color: #CC00FF;
-}
-
-#email-text,
-#email-email {
-    color: white;
-    background-color: #6600FF;
-}
-
-input.form-btn {
-    color: #CC00FF;
-    background-color: #99CCFF;
-    border: blanchedalmond solid .15em;
-}
-
-input.form-btn:hover {
-    background-color: #6600FF;
-    color: #66CCFF;
-}
-
-
-/* about us page styling */
-
-div.about-us-left-panel {
-    border: solid #6600FF .2em;
-    background-color: #CC00FF;
-}
-
-div.about-us-right-panel {
-    border: solid #6600FF .2em;
-    background-color: #CC00FF;
-}
-
-div.about-us-container {
-    border: solid #6600FF .2em;
-    background-color: #CC00FF;
-}
-
-
-/* footers */
-
-footer {
-    color: blanchedalmond;
-    background-color: #6600FF;
 }
