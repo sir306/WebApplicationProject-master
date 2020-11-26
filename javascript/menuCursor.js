845dfdf27aa830684ffc387628906bdc2e3659ce
Nicholas Richard Harding <20200344@learn.cpp.ac.nz>
2020-11-26 19:26:13 +1300

    final changes at course

--- javascript/menuCursor.js
+++ javascript/menuCursor.js
@@ -1,32 +1,51 @@
-var customCursor = ['https://cur.cursors-4u.net/nature/nat-10/nat979.ani' ,'http://cur.cursors-4u.net/nature/nat-10/nat979.gif'];
-
-var x = 0;
-
-function displayNextImage() {
-
-  document.querySelector("a.nav-link").style.cursor = 'url(https://cur.cursors-4u.net/nature/nat-10/nat979.ani), url(http://cur.cursors-4u.net/nature/nat-10/nat979.gif), auto !important';
-}
-displayNextImage();
-// const menuLink = document.querySelectorAll("a.nav-link");
-
-// function changeCursor(link) {
-//     link.style.cursor = "url(https://cur.cursors-4u.net/nature/nat-10/nat970.ani), url(http://cur.cursors-4u.net/nature/nat-10/nat970.gif), auto !important";
-//     console.log(link.style.cursor)
-//     console.log("hello");
-    
-// }
-// function addingListeners() {
-//     console.log(menuLink);
-
-//     menuLink.forEach(link => {
-//         console.log(link.style);
-//         link.addEventListener("mouseover", changeCursor(link))
-//     });
-// }
-// addingListeners();
-// var customCursor = 'url("http://cur.cursors-4u.net/nature/nat-10/nat970.gif"), auto !important';
-// document.getElementById("firstlink").style.cursor = "none !important";
-// console.log(document.getElementById("firstlink").style.cursor)
-// document.getElementById("firstlink").style.cursor =  'url("http://cur.cursors-4u.net/nature/nat-10/nat970.gif"), auto !important';
-// console.log(document.getElementById("firstlink").style.cursor)
-///* Start http://www.cursors-4u.com */ a {cursor: url(https://cur.cursors-4u.net/nature/nat-10/nat970.ani), url(http://cur.cursors-4u.net/nature/nat-10/nat970.gif), auto !important;} /* End http://www.cursors-4u.com */
+//custom cursor
+//body cursor
+$(function() {
+    $("body").mousemove(function(e) {
+        $(".cursor1").show().css({
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "left": e.clientX,
+            "top": e.clientY
+        });
+    }).mouseout(function() {
+        $(".cursor1").hide();
+    });
+});
+//anchor cursor
+$(function() {
+    $("a").mousemove(function(e) {
+        $(".cursor2").show().css({
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "left": e.clientX,
+            "top": e.clientY
+        });
+    }).mouseout(function() {
+        $(".cursor2").hide();
+    });
+});
+//button cursor
+$(function() {
+    $("button").mousemove(function(e) {
+        $(".cursor2").show().css({
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "left": e.clientX,
+            "top": e.clientY
+        });
+        $(".cursor1").hide();
+    }).mouseout(function() {
+        $(".cursor2").hide();
+        $(".cursor1").show().css({
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "left": e.clientX,
+            "top": e.clientY
+        });
+    });
+});
