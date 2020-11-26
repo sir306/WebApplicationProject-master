c79e301cf1f4a3536d8810776e07563d31a5cb81
sir306 <hardingnick00@gmail.com>
2020-11-27 00:57:51 +1300

    only footer left

--- javascript/menuCursor.js
+++ javascript/menuCursor.js
@@ -1,51 +1,98 @@
-//custom cursor
-//body cursor
-$(function() {
-    $("body").mousemove(function(e) {
-        $(".cursor1").show().css({
-            "top": "50%",
-            "left": "50%",
-            "transform": "translate(-50%, -50%)",
-            "left": e.clientX,
-            "top": e.clientY
-        });
-    }).mouseout(function() {
-        $(".cursor1").hide();
+//custom cursors
+
+//clicker cursors
+$(function () {
+  $("a").mousemove(function (e) {
+    $(".cursor").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
     });
+  }).mouseout(function () {
+    $(".cursor").hide();
+  });
 });
-//anchor cursor
-$(function() {
-    $("a").mousemove(function(e) {
-        $(".cursor2").show().css({
-            "top": "50%",
-            "left": "50%",
-            "transform": "translate(-50%, -50%)",
-            "left": e.clientX,
-            "top": e.clientY
-        });
-    }).mouseout(function() {
-        $(".cursor2").hide();
+
+$(function () {
+  $("button").mousemove(function (e) {
+    $(".cursor").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
     });
+  }).mouseout(function () {
+    $(".cursor").hide();
+  });
 });
-//button cursor
-$(function() {
-    $("button").mousemove(function(e) {
-        $(".cursor2").show().css({
-            "top": "50%",
-            "left": "50%",
-            "transform": "translate(-50%, -50%)",
-            "left": e.clientX,
-            "top": e.clientY
-        });
-        $(".cursor1").hide();
-    }).mouseout(function() {
-        $(".cursor2").hide();
-        $(".cursor1").show().css({
-            "top": "50%",
-            "left": "50%",
-            "transform": "translate(-50%, -50%)",
-            "left": e.clientX,
-            "top": e.clientY
-        });
+//home
+$(function () {
+  $("p.home").mousemove(function (e) {
+    $(".cursor-p-home").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
     });
+  }).mouseout(function () {
+    $(".cursor-p-home").hide();
+  });
+});
+//about us
+$(function () {
+  $("p.about-us").mousemove(function (e) {
+    $(".cursor-p-about-us").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
+    });
+  }).mouseout(function () {
+    $(".cursor-p-about-us").hide();
+  });
+});
+//contact us
+$(function () {
+  $(".text").mousemove(function (e) {
+    $(".cursor-td-contact-us").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
+    });
+  }).mouseout(function () {
+    $(".cursor-td-contact-us").hide();
+  });
+});
+$(function () {
+  $("textarea").mousemove(function (e) {
+    $(".cursor-td-contact-us").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
+    });
+  }).mouseout(function () {
+    $(".cursor-td-contact-us").hide();
+  });
+});
+$(function () {
+  $("input.input-contact-us").mousemove(function (e) {
+    $(".cursor-input-contact-us").show().css({
+      "top": "50%",
+      "left": "50%",
+      "transform": "translate(-50%, -50%)",
+      "left": e.clientX,
+      "top": e.clientY
+    });
+  }).mouseout(function () {
+    $(".cursor-input-contact-us").hide();
+  });
 });
